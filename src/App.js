d4168df72b217dc606046a5161efb59689ee2c08
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-11-08 20:54:27 -0800

    updated phase2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import Categories from './components/Categories';
-// import Header from './components/header/header';
-// import StoreFront from './components/storefront';
-// import SimpleCart from './components/cart/simplecart';
-
-// const [ showCart, setShowCart ] = useState(false);
+// import Products from './components/Products';
+// import Footer from './components/Footer';
+// import { Box } from '@mui/material';
+// import SimpleCart from './components/SimpleCart';
 
 function App() {
   return (
     <>
-      <Categories />
       {/* <div className="App">
-      <Header setShowCart={setShowCart} />
-      {showCart ? <SimpleCart/> : <StoreFront/>}
+      <Box sx={{ p: 2 }}>
+        <Header />
+        <Box sx={{ float: 'right' }}>
+          <SimpleCart />
+        </Box>
+        <Products />
+        <Footer />
+      </Box>
     </div> */}
+      <Categories />
+     
     </>
   );
 }
--- src/components/cart/simplecart.js
+++ src/components/cart/simplecart.js
@@ -1,51 +1,51 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Card, CardMedia, CardContent, CardActions } from '@mui/material';
-import { Container, Typography, Button } from '@mui/material';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Card, CardMedia, CardContent, CardActions } from '@mui/material';
+// import { Container, Typography, Button } from '@mui/material';
 
-const SimpleCart = () => {
-  const dispatch = useDispatch();
-  const cart = useSelector(state => state.cart);
-  const products = useSelector(state => state.products).products;
+// const SimpleCart = () => {
+//   const dispatch = useDispatch();
+//   const cart = useSelector(state => state.cart);
+//   const products = useSelector(state => state.products).products;
 
-  const removeFromCart = (product) => {
-    dispatch({ type: 'remove_product', payload: { product } });
-    const updatedProduct = products.find(currProduct => currProduct.id === product.id);
-    updatedProduct.inventoryCount++;
-    dispatch({ type: 'update_product', payload: updatedProduct });
-  }
+//   const removeFromCart = (product) => {
+//     dispatch({ type: 'remove_product', payload: { product } });
+//     const updatedProduct = products.find(currProduct => currProduct.id === product.id);
+//     updatedProduct.inventoryCount++;
+//     dispatch({ type: 'update_product', payload: updatedProduct });
+//   }
 
-  return (
-    <>
-      <Container id='current-categories'>
-        <Typography variant="h2">Your Cart</Typography>
-        <Typography variant="h5">Total Items: {cart.numberOfItems}</Typography>
-      </Container>
-      <Container id='productsContainer'>
-        {
-          cart.productList.map(product => (
-            <Card sx={{ maxWidth: 345 }} key={product.id}>
-              <CardMedia
-                component="img"
-                height="140"
-                image={product.img}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">{product.display}</Typography>
-                <Typography variant="body2" color="text.secondary">Quantity: {product.amountInCart}</Typography>
-              </CardContent>
+//   return (
+//     <>
+//       <Container id='current-categories'>
+//         <Typography variant="h2">Your Cart</Typography>
+//         <Typography variant="h5">Total Items: {cart.numberOfItems}</Typography>
+//       </Container>
+//       <Container id='productsContainer'>
+//         {
+//           cart.productList.map(product => (
+//             <Card sx={{ maxWidth: 345 }} key={product.id}>
+//               <CardMedia
+//                 component="img"
+//                 height="140"
+//                 image={product.img}
+//                 alt="green iguana"
+//               />
+//               <CardContent>
+//                 <Typography gutterBottom variant="h5" component="div">{product.display}</Typography>
+//                 <Typography variant="body2" color="text.secondary">Quantity: {product.amountInCart}</Typography>
+//               </CardContent>
 
-              <CardActions>
-                <Button size="small" onClick={() => removeFromCart(product)}>Remove</Button>
-              </CardActions>
-            </Card>
-          ))
-        }
-      </Container>
-    </>
-  )
-}
+//               <CardActions>
+//                 <Button size="small" onClick={() => removeFromCart(product)}>Remove</Button>
+//               </CardActions>
+//             </Card>
+//           ))
+//         }
+//       </Container>
+//     </>
+//   )
+// }
 
-export default SimpleCart;
+// export default SimpleCart;
 
 
